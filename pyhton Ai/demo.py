120c00210c42fd09543c7f042aa2be87e17cae17
nithingangula123-blip <nithingangula123@gmail.com>
2026-07-26 23:16:33 +0530

    Update demo.py

--- pyhton Ai/demo.py
+++ pyhton Ai/demo.py
@@ -1,3 +1,54 @@
+# Python Data Types Example
+
+# Integer
+age = 20
+
+# Float
+height = 5.9
+
+# Complex
+number = 3 + 4j
+
+# Boolean
+is_student = True
+
+# String
 name = "Nithin"
 
-print(f"Hello, {name}")
+# List
+fruits = ["Apple", "Banana", "Mango"]
+
+# Tuple
+colors = ("Red", "Green", "Blue")
+
+# Set
+numbers = {10, 20, 30}
+
+# Dictionary
+student = {
+    "name": "Nithin",
+    "age": 20,
+    "course": "B.Tech IT"
+}
+
+print("Integer:", age)
+print("Float:", height)
+print("Complex:", number)
+print("Boolean:", is_student)
+print("String:", name)
+print("List:", fruits)
+print("Tuple:", colors)
+print("Set:", numbers)
+print("Dictionary:", student)
+
+# Display data types
+print("\nData Types")
+print(type(age))
+print(type(height))
+print(type(number))
+print(type(is_student))
+print(type(name))
+print(type(fruits))
+print(type(colors))
+print(type(numbers))
+print(type(student))
